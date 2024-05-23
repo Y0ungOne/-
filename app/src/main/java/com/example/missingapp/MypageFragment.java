1647a0bae424479a085f531610ee7f68ca8feca7
안수연 <yun1035a@gmail.com>
2024-05-23 18:44:57 +0900

    ui만지던 중

---
+++ app/src/main/java/com/example/missingapp/MypageFragment.java
@@ -0,0 +1,20 @@
+package com.example.missingapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class MypageFragment extends Fragment {
+
+    public MypageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mypage, container, false);
+    }
+}
